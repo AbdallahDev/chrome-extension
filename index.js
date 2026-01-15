b15600721f3eca76ca24ceebc1304131e3fb3773
AbdallahDev <abdallah.dev85@gmail.com>
2026-01-15 10:51:09 +0000

    stage

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 const saveBtnEl = document.getElementById("inputBtn");
 const inputTxtEl = document.getElementById("inputTxt");
 const myLeads = [];
-const leadsDivEl = document.getElementById("leadsDiv");
+const leadsListEl = document.getElementById("leadsList");
+leadsListEl.style.listStyleType = "none";
 
 saveBtnEl.addEventListener("click", function () {
   myLeads.push(inputTxtEl.value);
   outputLead();
+  //   inputTxtEl.value = ""
 });
 
 function outputLead() {
+  const liEl = document.createElement("li");
+  leadsListEl.append(liEl);
   const anchorEl = document.createElement("a");
   lead = myLeads.at(-1);
   anchorEl.href = lead;
   anchorEl.target = "_blank";
-  const anchorTxt = document.createTextNode(lead);
-  anchorEl.appendChild(anchorTxt);
-  leadsDivEl.appendChild(anchorEl);
-
-  const breakEl = document.createElement("br");
-  leadsDivEl.appendChild(breakEl);
+  anchorEl.textContent = lead
+//   const anchorTxt = document.createTextNode(lead);
+//   anchorEl.appendChild(anchorTxt);
+//   liEl.appendChild(anchorEl);
+  liEl.append(anchorEl);
 }
 
 //box div code
 const boxEl = document.getElementById("box");
 boxEl.addEventListener("click", function () {
   console.log("I want to open the box!");
+});
+
+const containerEl = document.getElementById("container");
+containerEl.innerHTML =
+  '<button id="buyBtn" class="styling">' + "Buy" + "</button>";
+
+const buyBtnEl = document.getElementById("buyBtn");
+buyBtnEl.addEventListener("click", function () {
+  containerEl.innerHTML += "<p>Thank you for buying!</p>";
 });
