2f612b0367029afa77aaf6b1afcd19f5317ec1c9
AbdallahDev <abdallah.dev85@gmail.com>
2026-01-15 09:31:27 +0000

    stage

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 const saveBtnEl = document.getElementById("inputBtn");
 const inputTxtEl = document.getElementById("inputTxt");
+const myLeads = [];
+const leadsDivEl = document.getElementById("leadsDiv");
 
 saveBtnEl.addEventListener("click", function () {
-  console.log(inputTxtEl.value);
+  myLeads.push(inputTxtEl.value);
+  outputLead();
 });
 
+function outputLead() {
+  const anchorEl = document.createElement("a");
+  lead = myLeads.at(-1);
+  anchorEl.href = lead;
+  const anchorTxt = document.createTextNode(lead);
+  anchorEl.appendChild(anchorTxt);
+  leadsDivEl.appendChild(anchorEl);
+  const breakEl = document.createElement("br");
+  anchorEl.target = "_blank";
+  leadsDivEl.appendChild(breakEl);
+}
+
 //box div code
-const boxEl = document.getElementById('box')
-boxEl.addEventListener('click',function(){
-    console.log("I want to open the box!")
-})
+const boxEl = document.getElementById("box");
+boxEl.addEventListener("click", function () {
+  console.log("I want to open the box!");
+});
