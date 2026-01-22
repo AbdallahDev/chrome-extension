f4a73f23d68909918ff5a2073cee4eb2f885055a
AbdallahDev <abdallah.dev85@gmail.com>
2026-01-22 07:46:42 +0000

    stage

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const inputTxtEl = document.getElementById("inputTxt");
 let myLeads = JSON.parse(localStorage.getItem("myLeads")) ?? [];
 const leadsListEl = document.getElementById("leadsList");
 const clearBtnEl = document.getElementById("clearBtn");
+const saveTabBtnEl = document.getElementById("saveTabBtn");
+
+const tabs = [{ url: "https://github.com/AbdallahDev" }];
 
 saveBtnEl.addEventListener("click", function () {
-  myLeads.push(inputTxtEl.value);
-  render(myLeads);
-  inputTxtEl.value = "";
-
-  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  addNewValue(myLeads, "myLeads", inputTxtEl.value);
 });
 
 function render(leads) {
@@ -27,14 +26,6 @@ function render(leads) {
 const boxEl = document.getElementById("box");
 boxEl.addEventListener("click", function () {
   console.log("I want to open the box!");
-});
-
-const containerEl = document.getElementById("container");
-containerEl.innerHTML = `<button id="buyBtn" class="styling">Buy</button>`;
-
-const buyBtnEl = document.getElementById("buyBtn");
-buyBtnEl.addEventListener("click", function () {
-  containerEl.innerHTML += "<p>Thank you for buying!</p>";
 });
 
 //will rebuilds the leads list after each refresh
@@ -58,4 +49,61 @@ clearBtnEl.addEventListener("dblclick", () => {
   localStorage.clear();
   myLeads = [];
   renderList(myLeads);
+});
+
+const nameEl = document.getElementById("name");
+const greetTxtEl = document.getElementById("greetTxt");
+
+function greeting(greetTxt, name) {
+  const msg = `${greetTxt}, ${name}`;
+  console.log(msg);
+}
+
+const showMsgEl = document.getElementById("showMsg");
+showMsgEl.addEventListener("click", () => {
+  greeting(greetTxtEl.value, nameEl.value);
+});
+
+function getFirst(arry, indx) {
+  return arry[indx];
+}
+
+saveTabBtnEl.addEventListener("click", () => {
+  // async function getCurrentTabUrl() {
+  //   // Query for the active tab in the current window
+  //   let queryOptions = { active: true, currentWindow: true };
+  //   // The tabs array will contain one Tab object
+  //   let [tab] = await chrome.tabs.query(queryOptions);
+  //   return tab.url;
+  // }
+
+  // // Example usage
+  // getCurrentTabUrl().then((url) => {
+  //   addNewValue(myLeads, "myLeads", url);
+  // });
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    addNewValue(myLeads, "myLeads", tabs[0].url);
+  });
+});
+
+function addNewValue(valuesArray, arrayName, value) {
+  valuesArray.push(value);
+  render(valuesArray);
+
+  localStorage.setItem(arrayName, JSON.stringify(valuesArray));
+
+  inputTxtEl.value = "";
+}
+
+async function getCurrentTabUrl() {
+  // Query for the active tab in the current window
+  let queryOptions = { active: true, currentWindow: true };
+  // The tabs array will contain one Tab object
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab.url;
+}
+
+// Example usage
+getCurrentTabUrl().then((url) => {
+  console.log("Current Tab URL: " + url);
 });
